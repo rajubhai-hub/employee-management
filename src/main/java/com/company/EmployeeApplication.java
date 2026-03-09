743ec3a445e35c5d109221baf39ff413f4921380
rajesh <bankarajesh2308@gmail.com>
2026-03-09 15:13:28 +0530

    Added project files

---
+++ src/main/java/com/company/EmployeeApplication.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class EmployeeApplication {
+    public static void main(String[] args) {
+        System.out.println("Employee Management Application Running");
+    }
+}
